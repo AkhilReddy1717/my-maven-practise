0b4013d16d7dc5de331972279dbabe5cc5a2501e
AkhilReddy1717 <akhilreddy1717@gmail.com>
2021-04-03 06:36:13 +0000

    Maven project first commit

---
+++ src/main/java/com/mypractise/app/App.java
@@ -0,0 +1,13 @@
+package com.mypractise.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello all!" );
+    }
+}
